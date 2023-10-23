67692b26208a8d212f7c0208b441d62544434bce
Ruben <ruben1984@hotmail.com>
2023-10-23 20:00:56 +0200

    Interfaz mejorada con toolbars y otras utilidades.

--- app/src/main/java/com/example/construccion_layouts/MainActivity.java
+++ app/src/main/java/com/example/construccion_layouts/MainActivity.java
@@ -4,8 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,6 +30,25 @@ public class MainActivity extends AppCompatActivity {
         Intent i = new Intent(this, NewPlayer.class);
         startActivity(i);
     }
-}
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_search) {
+            Toast.makeText(this, "Buscar", Toast.LENGTH_SHORT).show();
+            // Acción a realizar cuando se hace clic en "action_search"
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+        }
+    }
+
+
 
 
